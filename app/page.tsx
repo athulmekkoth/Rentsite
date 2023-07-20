7a8881ca637ba9418bdf50dec6a05e4231e8dc68
Athul Suresh <athulmekkoth21@gmail.com>
2023-07-20 14:42:59 +0530

    front

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
-import { Hero } from './index'
+import { Hero } from '../components/index'
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
-     
+      <div className="mt-12 padding-x padding-y max-width" id="disocver">
+
+      </div>
+   
     </main>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-export default Hero
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="hero ">
+      <div className="flex-1 pt-36 padding-x ">
+        <h1 className="hero__title">
+          Find Your Dream House Now 
+        </h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+<div>
+  
+</div>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="xl: mt-32 xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
+        <div className= "relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
+        <Image className="rounded-xl" src="/home.jpg" width={600} height={400} alt="home"/>
+        </div>
+
+       
+      
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
